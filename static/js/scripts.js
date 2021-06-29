5b708701973aa67f5dc70f46df991bbfb5816aad
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-06-29 18:08:30 +0300

    services pages

---
+++ static/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.event-list').carousel({
+      interval: 2000
+    })
+  });  
+  
+  $(document).ready(function(){
+    $('.service-list').carousel({
+      interval: false
+    })
+  });    
